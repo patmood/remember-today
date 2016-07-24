df791b225e31d449e19018c5dcae091e6277e1e9
patmood <patrick.n.moody@gmail.com>
2016-07-23 18:49:05 -0700

    remove router for now

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -3,35 +3,24 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { increment } from '../actions/baseActions'
 
-import { Link } from 'react-context-router'
-
-const NotFound = () => <div>Not Found</div>
+import Home from '../components/Home'
 
 class AppContainer extends React.Component {
   render () {
-    const { route } = this.context
     const { base, actions } = this.props
-    const Comp = route.component || NotFound
 
     return (
       <div className='p2'>
         <h1>AppContainer</h1>
         <nav className='mb2'>
-          <Link href='/' className='btn btn-primary mr1'>Home</Link>
-          <Link href='/app' className='btn btn-primary mr1'>App</Link>
           <button onClick={actions.increment} className='btn'>+1 ({base.counter})</button>
         </nav>
         <div>
-          <Comp />
+          <Home />
         </div>
       </div>
     )
   }
-}
-
-AppContainer.contextTypes = {
-  history: PropTypes.object,
-  route: PropTypes.object
 }
 
 function mapStateToProps(state, ownProps) {
--- src/entry.js
+++ src/entry.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router } from 'react-context-router'
 import { Provider } from 'react-redux';
 
 import firebase from 'firebase'
 import firebaseConfig from '../firebase-config.json'
 
 import AppContainer from './containers/AppContainer'
-import Home from './components/Home'
-import App from './components/App'
 import './css/index.scss'
 
 // Store
@@ -16,22 +13,9 @@ import initialState from './reducers/initialState';
 import configureStore from './configureStore';
 const store = configureStore(initialState);
 
-const routes = [
-  {
-    path: '/',
-    component: Home,
-  },
-  {
-    path: '/app',
-    component: App,
-  },
-]
-
 ReactDOM.render(
   <Provider store={store}>
-    <Router routes={routes}>
     <AppContainer />
-    </Router>
   </Provider>,
   document.getElementById('root')
 )
